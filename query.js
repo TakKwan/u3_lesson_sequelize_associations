d9d8fb6d19443bb79506de071953a64ffd8bd25d
TakKwan <waslf3651@gmail.com>
2022-04-11 15:20:52 -0400

    done

--- query.js
+++ query.js
@@ -1,16 +1,24 @@
 const { User, Task, sequelize } = require('./models')
 
-// Find all users with their associated tasks
-// Raw SQL: SELECT * FROM "Users" JOIN tasks ON "Tasks"."userId" = "Users".id;
+const stringify = (data) => {
+  console.log(JSON.stringify(data, null, 2))
+}
 
 const findAllWithTasks = async () => {
   // Find all users with their associated tasks
   // Raw SQL: SELECT * FROM users JOIN tasks ON tasks."userId" = users.id;
+  const res = await User.findAll({ include: [{ model: Task, required: true }] })
+  stringify(res)
 }
 
 const findAllJohnsWithTasks = async () => {
   // Find all users with their associated tasks
   // Raw SQL: SELECT * FROM users JOIN tasks ON tasks."userId" = users.id WHERE users."firstName" = 'John';
+  const res = await User.findAll({
+    where: { firstName: 'John' },
+    include: [{ model: Task, required: true }]
+  })
+  stringify(res)
 }
 
 const run = async () => {
